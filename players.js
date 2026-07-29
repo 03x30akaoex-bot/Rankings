cc31a3874dc435880090fd0449ccfca100390cef
AbilityRankings <03x30akaoex@gmail.com>
2026-07-30 01:01:54 +0200

    Create players.js

---
+++ players.js
@@ -0,0 +1,21 @@
+const players = [
+  {
+    userId: 123456789,
+    stats: {
+      CC: 9.8,
+      CAM: 9.1,
+      BNC: 8.9,
+      CONS: 9.6
+    }
+  },
+
+  {
+    userId: 987654321,
+    stats: {
+      CC: 8.4,
+      CAM: 9.0,
+      BNC: 7.8,
+      CONS: 8.6
+    }
+  }
+];
